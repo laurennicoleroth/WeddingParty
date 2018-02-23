a21063ae1166cc613b7a2eaf94ccbab8d949b2c1
Lauren N. Roth <laurennicoleroth@gmail.com>
2018-02-23 13:13:35 -0500

    Extend uibutton

--- WeddingParty/Controllers/LoginViewController.swift
+++ WeddingParty/Controllers/LoginViewController.swift
@@ -15,10 +15,13 @@ class LoginViewController: UIViewController {
   @IBOutlet weak var passwordField: UITextField!
   @IBOutlet weak var dismissButton: UIButton!
   
+  var continueButton:RoundedWhiteButton!
+  var activityView:UIActivityIndicatorView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    uiSetup()
   }
   
   override func didReceiveMemoryWarning() {
@@ -26,15 +29,32 @@ class LoginViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  func uiSetup() {
+    
+    continueButton = RoundedWhiteButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+    continueButton.setTitleColor(secondaryColor, for: .normal)
+    continueButton.setTitle("Continue", for: .normal)
+    continueButton.titleLabel?.font = UIFont.systemFont(ofSize: 18.0, weight: UIFont.Weight.bold)
+    continueButton.center = CGPoint(x: view.center.x, y: view.frame.height - continueButton.frame.height - 24)
+    continueButton.highlightedColor = UIColor(white: 1.0, alpha: 1.0)
+    continueButton.defaultColor = UIColor.white
+    continueButton.addTarget(self, action: #selector(handleSignIn), for: .touchUpInside)
+    continueButton.alpha = 0.5
+    view.addSubview(continueButton)
+    setContinueButton(enabled: false)
+    
+    activityView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+    activityView.color = secondaryColor
+    activityView.frame = CGRect(x: 0, y: 0, width: 50.0, height: 50.0)
+    activityView.center = continueButton.center
+    
+    view.addSubview(activityView)
+    
+    emailField.delegate = self
+    passwordField.delegate = self
+    
+    emailField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+    passwordField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+  }
   
 }

d5beb14e222018d65cb605d7cc0368c6a4b654bc
Lauren N. Roth <laurennicoleroth@gmail.com>
2018-02-23 11:24:15 -0500

    Add initial project files, no pods

---
+++ WeddingParty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeddingParty
+//
+//  Created by Lauren Nicole Roth on 2/23/18.
+//  Copyright © 2018 Lauren Nicole Roth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ WeddingPartyTests/WeddingPartyTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeddingPartyTests.swift
+//  WeddingPartyTests
+//
+//  Created by Lauren Nicole Roth on 2/23/18.
+//  Copyright © 2018 Lauren Nicole Roth. All rights reserved.
+//
+
+import XCTest
+@testable import WeddingParty
+
+class WeddingPartyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

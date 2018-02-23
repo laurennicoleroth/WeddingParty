aceea12504fa3ddb6ad1f39eafa2e018916d88ab
Lauren N. Roth <laurennicoleroth@gmail.com>
2018-02-23 13:18:49 -0500

    Flesh out login screen

--- WeddingParty/Controllers/LoginViewController.swift
+++ WeddingParty/Controllers/LoginViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController:UIViewController, UITextFieldDelegate {
   
   @IBOutlet weak var emailField: UITextField!
   @IBOutlet weak var passwordField: UITextField!
@@ -20,16 +20,6 @@ class LoginViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    uiSetup()
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  func uiSetup() {
     
     continueButton = RoundedWhiteButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
     continueButton.setTitleColor(secondaryColor, for: .normal)
@@ -57,4 +47,107 @@ class LoginViewController: UIViewController {
     passwordField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
   }
   
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    emailField.becomeFirstResponder()
+    NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillAppear), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    emailField.resignFirstResponder()
+    passwordField.resignFirstResponder()
+    NotificationCenter.default.removeObserver(self)
+  }
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    get {
+      return .lightContent
+    }
+  }
+  
+  @IBAction func handleDismissButton(_ sender: Any) {
+    self.dismiss(animated: false, completion: nil)
+  }
+  
+  /**
+   Adjusts the center of the **continueButton** above the keyboard.
+   - Parameter notification: Contains the keyboardFrame info.
+   */
+  
+  @objc func keyboardWillAppear(notification: NSNotification){
+    
+    let info = notification.userInfo!
+    let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+    
+    continueButton.center = CGPoint(x: view.center.x,
+                                    y: view.frame.height - keyboardFrame.height - 16.0 - continueButton.frame.height / 2)
+    activityView.center = continueButton.center
+  }
+  
+  /**
+   Enables the continue button if the **username**, **email**, and **password** fields are all non-empty.
+   
+   - Parameter target: The targeted **UITextField**.
+   */
+  
+  @objc func textFieldChanged(_ target:UITextField) {
+    let email = emailField.text
+    let password = passwordField.text
+    let formFilled = email != nil && email != "" && password != nil && password != ""
+    setContinueButton(enabled: formFilled)
+  }
+  
+  
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    
+    // Resigns the target textField and assigns the next textField in the form.
+    
+    switch textField {
+    case emailField:
+      emailField.resignFirstResponder()
+      passwordField.becomeFirstResponder()
+      break
+    case passwordField:
+      handleSignIn()
+      break
+    default:
+      break
+    }
+    return true
+  }
+  
+  /**
+   Enables or Disables the **continueButton**.
+   */
+  
+  func setContinueButton(enabled:Bool) {
+    if enabled {
+      continueButton.alpha = 1.0
+      continueButton.isEnabled = true
+    } else {
+      continueButton.alpha = 0.5
+      continueButton.isEnabled = false
+    }
+  }
+  
+  @objc func handleSignIn() {
+    guard let email = emailField.text else { return }
+    guard let pass = passwordField.text else { return }
+    
+    setContinueButton(enabled: false)
+    continueButton.setTitle("", for: .normal)
+    activityView.startAnimating()
+    
+    Auth.auth().signIn(withEmail: email, password: pass) { user, error in
+      if error == nil && user != nil {
+        self.dismiss(animated: false, completion: nil)
+      } else {
+        print("Error logging in: \(error!.localizedDescription)")
+      }
+    }
+  }
 }
--- WeddingParty/RoundedWhiteButton.swift
+++ WeddingParty/RoundedWhiteButton.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class RoundedWhiteButton:UIButton {
   

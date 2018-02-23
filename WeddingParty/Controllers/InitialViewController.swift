f8bc0ffa6f1bf251d6576dc4c8d1698dc1699cd5
Lauren N. Roth <laurennicoleroth@gmail.com>
2018-02-23 12:28:19 -0500

    Add initial vc

---
+++ WeddingParty/Controllers/InitialViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InitialViewController.swift
+//  WeddingParty
+//
+//  Created by Lauren Nicole Roth on 2/23/18.
+//  Copyright © 2018 Lauren Nicole Roth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InitialViewController: UIViewController {
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    self.performSegue(withIdentifier: "toMenuScreen", sender: self)
+  }
+}
